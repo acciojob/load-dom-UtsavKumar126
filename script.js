fb466c8808f5e1962bd2bfd447e22d16acf8d014
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-19 18:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 //your JS code here. If required.
-        document.addEventListener("DOMContentLoaded", function() {
-        // Create a new paragraph element
-        var paragraph = document.createElement("p");
-        
-        // Set the text content of the paragraph to "DOM load success"
-        paragraph.textContent = "DOM load success";
-
-        // Append the paragraph to the body tag
-        document.body.appendChild(paragraph);
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.innerText="DOM load success";
+});
